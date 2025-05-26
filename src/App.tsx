bf2156aca0e8b839d8b3c41152a197126e3e5d6f
foxmon <jjockrod@naver.com>
2025-05-26 18:16:47 +0900

    [KAN-51] toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
+import { Bounce, ToastContainer } from "react-toastify";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { HoBomRouter } from "@/apps/router";
 import { theme } from "@/shared/style/config";
 
+import "react-toastify/dist/ReactToastify.css";
+
+const TOAST_AUTO_CLOSE_MS = 3000;
 const queryClient = new QueryClient();
 
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
+      <ToastContainer
+        position="bottom-center"
+        hideProgressBar
+        newestOnTop
+        closeOnClick
+        pauseOnHover
+        transition={Bounce}
+        autoClose={TOAST_AUTO_CLOSE_MS}
+      />
       <QueryClientProvider client={queryClient}>
         <HoBomRouter />
       </QueryClientProvider>
