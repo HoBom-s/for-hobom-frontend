4d1be7d663cccaac7f18150c0e5cf70b2cfed717
foxmon <jjockrod@naver.com>
2025-05-26 17:48:15 +0900

    [KAN-49] theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { HoBomRouter } from "@/apps/router";
+import { theme } from "@/shared/style/config";
 
 const queryClient = new QueryClient();
 
 export default function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <HoBomRouter />
-    </QueryClientProvider>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <QueryClientProvider client={queryClient}>
+        <HoBomRouter />
+      </QueryClientProvider>
+    </ThemeProvider>
   );
 }
