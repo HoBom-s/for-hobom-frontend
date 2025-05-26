b1ac6bfeed83a20a484e824eca8bb99830d2bce7
JunHo <jjockrod@naver.com>
2025-05-26 18:06:41 +0900

    Merge pull request #1 from HoBom-s/hb-frontend/kan-49
    feat: [KAN-49] theme

---
+++ src/shared/style/config/theme.config.ts
@@ -0,0 +1,26 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  breakpoints: {
+    values: {
+      xs: 0, // mobile
+      sm: 600, // tablet portrait
+      md: 900, // tablet landscape
+      lg: 1200, // desktop
+      xl: 1536, // large screen
+    },
+  },
+  typography: {
+    fontFamily: `'Roboto', 'Helvetica', 'Arial', sans-serif`,
+    h6: {
+      fontSize: "1.125rem",
+    },
+    body1: {
+      fontSize: "1rem",
+    },
+    button: {
+      textTransform: "none",
+    },
+  },
+  spacing: 8,
+});
