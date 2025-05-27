1bbd371a2f07eb321a294005e3a63163a098b9c9
foxmon <jjockrod@naver.com>
2025-05-27 12:24:30 +0900

    [KAN-58] bom

---
+++ src/packages/core/types/iterable.type.ts
@@ -0,0 +1,6 @@
+export type Iterable<T = unknown> = ReadonlyArray<T> | readonly [];
+
+export type Mappable<T extends Iterable, K> = {
+  // Remove readonly property.
+  -readonly [P in keyof T]: K;
+};
