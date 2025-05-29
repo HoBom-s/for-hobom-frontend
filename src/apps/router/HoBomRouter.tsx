5c190e68b7ec63831442ba360788732d090f5c6c
JunHo <jjockrod@naver.com>
2025-05-29 14:30:38 +0900

    [KAN-62] not found page (#10)

--- src/apps/router/HoBomRouter.tsx
+++ src/apps/router/HoBomRouter.tsx
@@ -1,18 +1,37 @@
 import { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
+import { CircularProgress } from "@mui/material";
 import { RoutesConfig } from "@/shared/routes/config/routes.config.ts";
 
 const DailyTodoPage = lazy(() => import("@/pages/daily-todo"));
+const NotFoundPage = lazy(() => import("@/pages/not-found"));
 
 export const HoBomRouter = () => {
   return (
-    <Suspense fallback={null}>
+    <Suspense fallback={<HoBomRouter.Loader />}>
       <Routes>
         <Route
           path={RoutesConfig.MAIN.DAILY_TODO}
           element={<DailyTodoPage />}
         />
+        <Route path={RoutesConfig.NOT_FOUND.ALL} element={<NotFoundPage />} />
       </Routes>
     </Suspense>
+  );
+};
+
+HoBomRouter.Loader = () => {
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <CircularProgress />
+    </div>
   );
 };
