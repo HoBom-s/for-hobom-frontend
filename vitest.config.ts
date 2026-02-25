28f86423a8fd12cf0e78e50705c23848f882c246
foxmon <jjockrod@naver.com>
2026-02-25 13:00:20 +0900

    note

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,7 +6,7 @@ export default mergeConfig(
   defineConfig({
     test: {
       globals: true,
-      environment: "jsdom",
+      environment: "happy-dom",
       include: ["src/**/*.{spec,test}.{ts,tsx}"],
     },
   }),
